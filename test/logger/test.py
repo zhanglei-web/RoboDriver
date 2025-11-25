9db1090d05e0be1472c43c60ac66e0e2d6f304d1
Xiang Yang <Ryu-Yang@qq.com>
2025-11-25 16:19:35 +0800

    chore(test):  remove log test files

--- test/logger/test.py
+++
@@ -1,8 +0,0 @@
-# your_script.py
-from colored_logging import setup_colored_logger
-
-logger = setup_colored_logger(__name__)
-
-logger.info("启动程序 ✅")
-logger.warning("注意：内存使用较高 ⚠️")
-logger.error("发生错误 ❌")
